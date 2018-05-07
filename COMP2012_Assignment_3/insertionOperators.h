757a6c5f17f6155d7aeea849fcb0c835b839fd2c
Elliot Ng <chihimng.io@gmail.com>
2018-05-07 21:59:43 +0800

    feat: implement insertionOperators

--- COMP2012_Assignment_3/insertionOperators.h
+++ COMP2012_Assignment_3/insertionOperators.h
@@ -2,3 +2,15 @@
 //also implement the insertion operators (i.e. operator<<) for the HashingVault class in this file
 //their implementations should call the print function of the class
 //hint: don't need to include any header in this file
+
+template <typename KeyType, typename ValueType>
+ostream& operator<<(ostream& os, const Container<KeyType, ValueType>& container) {
+    container.print(os);
+    return os;
+}
+
+template <typename KeyType, typename ValueType>
+ostream& operator<<(ostream& os, const HashingVault<KeyType, ValueType>& hashingVault) {
+    hashingVault.print(os);
+    return os;
+}
